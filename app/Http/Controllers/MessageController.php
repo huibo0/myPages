ab51faecb02c826d6169c70b2b6f91f741bcab7c
惠博 <huibo@bluereader.cn>
2018-05-07 15:25:43 +0800

    t

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -43,7 +43,7 @@ class MessageController extends Controller
             }
 
         } catch (\Exception $exception) {
-            dd($exception);
+            dd($exception->getTrace());
             die('出现错误');
         }
     }
