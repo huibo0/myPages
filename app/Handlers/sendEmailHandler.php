97457b4f75908680e7e47ecf0059c6c8a2e57b66
惠博 <huibo@bluereader.cn>
2018-05-07 14:50:35 +0800

    t

--- app/Handlers/sendEmailHandler.php
+++ app/Handlers/sendEmailHandler.php
@@ -11,7 +11,7 @@ class sendEmailHandler
 
 
         // Mail::send()的返回值为空，所以可以其他方法进行判断
-        Mail::send('email',['name' => $name , 'content' => $content , 'email' => $email],function($message) use ($email){
+        Mail::send('email',['name' => $name ],function($message) use ($email){
             $to = $email; //邮件地址
             $message ->to($to)->subject('留言提醒'); //邮件名
         });
