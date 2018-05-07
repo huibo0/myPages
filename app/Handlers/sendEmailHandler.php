31c7ef7ad1d3c97edc1271df34e75d98e4e63a24
惠博 <huibo@bluereader.cn>
2018-05-07 14:47:54 +0800

    t

--- app/Handlers/sendEmailHandler.php
+++ app/Handlers/sendEmailHandler.php
@@ -6,12 +6,12 @@ use Mail;
 class sendEmailHandler
 {
 
-    public function send($name,$email,$message)
+    public function send($name,$email,$content)
     {
 
 
         // Mail::send()的返回值为空，所以可以其他方法进行判断
-        Mail::send('email',['name' => $name , 'content' => $message , 'email' => $email],function($message) use ($email){
+        Mail::send('email',['name' => $name , 'content' => $content , 'email' => $email],function($message) use ($email){
             $to = $email; //邮件地址
             $message ->to($to)->subject('留言提醒'); //邮件名
         });
